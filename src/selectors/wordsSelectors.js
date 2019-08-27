1a266eacf8af09b9dd8904eaf4f9ec1653dcdc2d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-27 14:35:27 -0700

    had to refactor my initialState within my wordsReducer so it is an object with different properties

--- src/selectors/wordsSelectors.js
+++ src/selectors/wordsSelectors.js
@@ -1,7 +1,4 @@
-export const getAllWords = state => state.dictionary;
+export const getAllWords = state => state.words.dictionary;
 export const getWordsByCount = (state, count = 1000) => {
   return getAllWords(state).slice(0, count); 
 };
-
-//need to look at source code for using match and plug that in as
-//the second argument to my selector in TopWords
