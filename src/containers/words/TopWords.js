9b4f108ed9c9ebca6bbe24d099ce2dd9e962bc68
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-27 13:42:35 -0700

    plugged my selector into my mapStateToProps in my container

--- src/containers/words/TopWords.js
+++ src/containers/words/TopWords.js
@@ -1,7 +1,10 @@
 import { connect } from 'react-redux';
 import Words from '../../components/words/Words';
+import { getWordsByCount } from '../../selectors/wordsSelectors';
 
-const mapStateToProps = state => ({ state }); 
+const mapStateToProps = state => ({
+  words: getWordsByCount(state, 1000)
+}); 
 
 export default connect(
   mapStateToProps
