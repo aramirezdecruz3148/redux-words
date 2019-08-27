18e0371f58bb7abb734714c618f47a7475259c0c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-27 14:23:23 -0700

    finished the functionality for the second route by adding match into my TopWords container

--- src/containers/words/TopWords.js
+++ src/containers/words/TopWords.js
@@ -2,8 +2,8 @@ import { connect } from 'react-redux';
 import Words from '../../components/words/Words';
 import { getWordsByCount } from '../../selectors/wordsSelectors';
 
-const mapStateToProps = state => ({
-  words: getWordsByCount(state)
+const mapStateToProps = (state, props) => ({
+  words: getWordsByCount(state, props.match.params.count)
 }); 
 
 export default connect(
--- src/selectors/wordsSelectors.js
+++ src/selectors/wordsSelectors.js
@@ -2,3 +2,6 @@ export const getAllWords = state => state.dictionary;
 export const getWordsByCount = (state, count = 1000) => {
   return getAllWords(state).slice(0, count); 
 };
+
+//need to look at source code for using match and plug that in as
+//the second argument to my selector in TopWords
