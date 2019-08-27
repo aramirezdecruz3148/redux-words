eaa326ed0900eb87a23b875d7f596d34fdc9e2f1
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-27 13:30:24 -0700

    created my TopWords container with an empty mapStateToProps for right now

---
+++ src/containers/words/TopWords.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import Words from '../../components/words/Words';
+
+const mapStateToProps = state => ({ state });
+
+export default connect(
+  mapStateToProps
+)(Words);
