349b1657e5e278b2d102d3b657f971a86f091e83
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-27 15:32:45 -0700

    created the selector getSearchTerm

--- src/selectors/wordsSelectors.js
+++ src/selectors/wordsSelectors.js
@@ -1,4 +1,7 @@
 export const getAllWords = state => state.words.dictionary;
+
 export const getWordsByCount = (state, count = 1000) => {
   return getAllWords(state).slice(0, count); 
 };
+
+export const getSearchTerm = state => state.words.searchTerm;
