b7deb01b383ec5d38fa0e3638b26633d6217f7b1
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-28 09:28:31 -0700

    created snapshots for all my non-container, non-page components

--- src/containers/words/SearchWords.js
+++ src/containers/words/SearchWords.js
@@ -3,9 +3,8 @@ import SearchForm from '../../components/words/SearchForm';
 import { getSearchTerm } from '../../selectors/wordsSelectors';
 import { updateSearchTerm } from '../../actions/wordsActions';
 
-const mapStateToProps = (state, /*props*/) => ({
+const mapStateToProps = (state) => ({
   searchTerm: getSearchTerm(state)
-  // props.history.push(`/?query=${searchTerm}`)
 });
 
 const mapDispatchToProps = dispatch => ({
